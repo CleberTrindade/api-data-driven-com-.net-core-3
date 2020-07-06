82870cfb1f9043ef17b66a34e5c8bfcfeb127f2e
jose.trindade <jose.trindade@inmetrics.com.br>
2020-07-05 22:27:02 -0300

    Ajuste connectionString.

--- Startup.cs
+++ Startup.cs
@@ -67,10 +67,10 @@ namespace Shop
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            if (env.IsDevelopment())
+                app.UseDeveloperExce
 
-            if (env.IsDevelopment())            
-                app.UseDeveloperExceptionPage();
-          
+
             app.UseHttpsRedirection();
 
             app.UseSwagger();
